eb0734feb08627d25b9fc28f6dfc2ded9271989f
Johannes <themeatpi@gmail.com>
2018-11-26 14:56:14 +0100

    Johannes

---
+++ src/CustomerMainTestJohannesA.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class CustomerMainTestJohannesA {
+
+
+
+    public static void main(String[] args) {
+        Customer firstCustomer = new Customer("Johannes", "Linde-Hansen", "My home", "12345678");
+
+        System.out.println(firstCustomer.toString());
+
+        Customer secondCustomer = new Customer("Johannes", "Linde-Hansen", "My home", "12345678");
+
+        System.out.println(secondCustomer.toString());
+
+        Customer thirdCustomer = new Customer("Johannes", "Linde-Hansen", "My home", "12345678");
+
+        System.out.println(thirdCustomer.toString());
+
+    }
+}
