e61ef4e5e0899c0f662c2e4c54ff0ca6cd74a5dd
Thomas <mail@thomasdahl.one>
2018-11-26 20:32:58 +0100

    Nu fra IntelliJ

--- src/CurrentAccount.java
+++ src/CurrentAccount.java
@@ -1,12 +1,12 @@
 public class CurrentAccount extends Account{
 
     private static final long STANDARD_OVERDRAFT_ALLOWED = 1_000_000; //10.000 kr
-    private static final int STANDARD_INTEREST_RATE = 10;
+    private static final int STANDARD_INTEREST_RATE = 10; //0.10%
     private static final long SERVICE_CHARGE = 10_000; //100 kr
-    
+
     /**
      * Constructor for the CurrentAccount Class.
-     * @param accountNo
+     * @param accountNo The account number.
      */
     public CurrentAccount(String accountNo) {
         super(accountNo);
@@ -14,10 +14,24 @@ public class CurrentAccount extends Account{
         this.interestRate = STANDARD_INTEREST_RATE;
         this.overdraftAllowed = STANDARD_OVERDRAFT_ALLOWED;
     }
-    
+
+    /**
+     * Handling withdrawal of money from accounts.
+     * @param amount The amount of money to be withdrawn.
+     */
     @Override
     public void withdraw(long amount) {
-        //Test
+        if(this.balance - amount < -this.overdraftAllowed) {
+            handleServiceCharge();
+        }
+        this.balance -= amount;
+    }
+
+    /**
+     * Handling the Service Charge when allowed overdraft is surpassed.
+     */
+    private void handleServiceCharge() {
+        //TODO - Make a transaction of amount "SERVICE_CHARGE" to the Bank's own account?
     }
 
 }
