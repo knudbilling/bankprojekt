94d115e6759c94962e587ceb7ecdcaf28123ce66
Knud Billing <knudjbilling@gmail.com>
2018-11-27 09:27:39 +0100

    MySQL

--- src/Persistance.java
+++ src/Persistance.java
@@ -1,16 +1,16 @@
 import java.util.List;
 
 public interface Persistance {
-    public boolean updateBank(Bank bank);
-    public boolean addCustomer(Customer customer);
-    public boolean updateCustomer(Customer customer);
-    public boolean addAccount(Account account);
-    public boolean updateAccount(Account account);
-    public boolean addTransaction(Transaction transaction);
-    public boolean saveAll(Bank bank);
+    boolean updateBank(Bank bank);
+    boolean addCustomer(Customer customer);
+    boolean updateCustomer(Customer customer);
+    boolean addAccount(Account account);
+    boolean updateAccount(Account account);
+    boolean addTransaction(Transaction transaction);
+    boolean saveAll(Bank bank);
 
-    public Bank loadBank();
-    public Bank loadAll();
+    Bank loadBank();
+    Bank loadAll();
 
-    public boolean resetPersistance();
+    boolean resetPersistance();
 }
