f8683b20d366b60646b73324b6a853fb6e639aa1
Johannes <themeatpi@gmail.com>
2018-11-27 11:03:27 +0100

    Johannes

--- src/Customer.java
+++ src/Customer.java
@@ -3,34 +3,36 @@ import java.util.*;
 
 public class Customer {
     // fields
-    private String firstName;
-    private String lastName;
-    private String address;
-    private String phoneNo;
-    private int idNo;
+    public String firstName;
+    public String lastName;
+    public String address;
+    public String phoneNo;
+    public int idNo;
+    public List<Account> accountList = new ArrayList<>();
 
     static int nextidNo = 0;
 
 
-   // private list[] accounts;
-
     // constructor
     public Customer(String firstName, String lastName, String address, String phoneNo){
+        // default constructor.
         this();
         this.firstName = firstName;
         this.lastName = lastName;
         this.address = address;
         this.phoneNo = phoneNo;
-
-
     }
+    //default constructor.
+    public Customer() {idNo = nextidNo++}
 
 
-    public Customer() {
-        idNo = nextidNo++;
+    //Methods
+    public int getidNo(){
+        return this.idNo;
     }
-
-
+    public void setidNo(){
+        this.idNo = idNo;
+    }
 
     public String toString() {
         return "first name: " + this.firstName
@@ -38,6 +40,11 @@ public class Customer {
                 + this.address + "phone number: " + this.phoneNo + " idNo: " + this.idNo;
     }
 
+    private void addAccount(Account){
+        // this.Account
+        accountListt.add(Account);
+
+    }
 
 
 
