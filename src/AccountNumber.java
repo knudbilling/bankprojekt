58e12ce9eaf671c13ea3f775d88db7525f1e4464
Knud Billing <knudjbilling@gmail.com>
2018-11-30 18:45:54 +0100

    oprydning

--- src/AccountNumber.java
+++ src/AccountNumber.java
@@ -6,36 +6,36 @@ public class AccountNumber {
 
     /**
      * Get the 4 digit registration number from a 14 digit account number.
-     * @param account Full account number (14 digits)
+     * @param accountNumber Full account number (14 digits)
      * @return Registration number (4 digits). Null is account number is malformed.
      */
-    public static String getRegistrationNumber(String account){
-        if(!AccountNumber.isValidFormat(account))
+    public static String getRegistrationNumber(String accountNumber){
+        if(!AccountNumber.isValidFormat(accountNumber))
             return null;
-        return account.substring(0,4);
+        return accountNumber.substring(0,4);
     }
 
     /**
      * Get the 10 digit account number from a 14 digit account number.
-     * @param account Full account number (14 digits)
+     * @param accountNumber Full account number (14 digits)
      * @return The account number (10 digits). Null if the account number is malformed.
      */
-    public static String getAccountNumber(String account){
-        if(!AccountNumber.isValidFormat(account))
+    public static String getAccountNumber(String accountNumber){
+        if(!AccountNumber.isValidFormat(accountNumber))
             return null;
-        return account.substring(4);
+        return accountNumber.substring(4);
     }
 
     /**
      * Tests that an account number has exactly 14 numeric digits.
-     * @param account Account number to test.
+     * @param accountNumber Account number to test.
      * @return True is account number is valid. Otherwise false.
      */
-    public static boolean isValidFormat(String account){
-        if(account.length()!=14)
+    public static boolean isValidFormat(String accountNumber){
+        if(accountNumber.length()!=14)
             return false;
-        for(int i=0;i<account.length();i++){
-            if(account.charAt(i)<48 || (account.charAt(i)>58))
+        for(int i=0;i<accountNumber.length();i++){
+            if(accountNumber.charAt(i)<48 || (accountNumber.charAt(i)>58))
                 return false;
         }
         return true;
@@ -44,42 +44,27 @@ public class AccountNumber {
     /**
      * Test if an account has the same registration number as a specific bank instance
      * @param bank The bank to check against
-     * @param account Full account number (14 digits)
+     * @param accountNumber Full account number (14 digits)
      * @return True if the account has the same registration number af the supplied bank. Otherwise false.
      */
-    public static boolean isLocal(Bank bank, String account){
-        String reg=AccountNumber.getRegistrationNumber(account);
-        if(reg==null)
+    public static boolean isLocal(Bank bank, String accountNumber){
+        String registrationNumber=AccountNumber.getRegistrationNumber(accountNumber);
+        if(registrationNumber==null)
             return false;
-        if(!reg.equals(bank.getRegNo()))
+        if(!registrationNumber.equals(bank.getRegNo()))
             return false;
         return true;
     }
 
     /**
-     * Tests whether an account exists in a specific bank instance
-     * @param bank The bank to check for the account
-     * @param account The account to check
-     * @return True is the account exists in the supplied bank. Otherwise false.
-     */
-    public static boolean existsInBank(Bank bank, String account){
-        String accountNumber=AccountNumber.getAccountNumber(account);
-       for(Account a:bank.getAccountList()){
-            if(a.accountNo.equals(accountNumber))
-                return true;
-        }
-        return false;
-    }
-
-    /**
      * Get an Account object identified by the supplied account from a bank intance
      * @param bank The bank to get the Account object from
-     * @param account The full account number (14 digits)
+     * @param accountNumber The full account number (14 digits)
      * @return The account object if it exists. Otherwise null.
      */
-    public static Account getAccount(Bank bank, String account){
+    public static Account getAccount(Bank bank, String accountNumber){
         for(int i=0;i<bank.getAccountList().size();i++){
-            if(bank.getAccountList().get(i).accountNo.equals(account))
+            if(bank.getAccountList().get(i).accountNo.equals(accountNumber))
                 return bank.getAccountList().get(i);
         }
         System.out.println("not found");
--- src/BankRegister.java
+++ src/BankRegister.java
@@ -1,8 +1,11 @@
 public class BankRegister {
+    static final String[] externalBankAccountNumbers = {"99000000000001", "99000000000002"};
 
-    static public boolean accountExists(String account){
-        if(account.equals("99000000000001"))
-            return true;
+    static public boolean accountExists(String accountNumber) {
+
+        for (int i = 0; i < externalBankAccountNumbers.length; i++)
+            if (externalBankAccountNumbers[i].equals(accountNumber))
+                return true;
         return false;
     }
 }
