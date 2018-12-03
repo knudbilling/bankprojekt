8feb4759e3938bbf55d2deccdea4566473f12737
Johannes <themeatpi@gmail.com>
2018-12-03 10:45:09 +0100

    Johannes

---
+++ test/JohannesTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+//import java.util.List;
+import java.util.*;
+
+import static org.junit.Assert.assertEquals;
+public class JohannesTest {
+
+    //Test 1 - Overførsel af gyldigt beløb fra opsparingskonto til egen lønkonto
+    @Test
+
+
+
+
+}
