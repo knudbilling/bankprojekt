ba01fa831360593cfc9cf0e4b924f8dabb1ae7af
Knud Billing <knudjbilling@gmail.com>
2018-12-06 21:38:02 +0100

    Testsuite

---
+++ test/TestSuite.java
@@ -0,0 +1,16 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        JohannesTest.class, // Test 1-3
+//        PatriciaTest.class, // Test 4-6
+        Martin_Test.class, // Test 7-9
+        JuliansTest.class, // Test 10-12
+        ThomasTest.class, // Test 13-15
+        KnudTest.class // Test 16-18
+
+})
+
+public class TestSuite {
+}
