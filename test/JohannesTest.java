f9ea8bce9bbe52a7757046ce55915aa762e68da6
Johannes <themeatpi@gmail.com>
2018-12-06 10:54:50 +0100

    Johannes

--- test/JohannesTest.java
+++ test/JohannesTest.java
@@ -9,7 +9,7 @@ public class JohannesTest {
 
     //Test 1 - Overførsel af gyldigt beløb fra opsparingskonto til egen lønkonto
     @Test
-    public void canMakeATransferFromSavingsToCurrentAccount() {
+    public void canMakeATransferFromSavingsToCurrentAccount() throws Exception{
         Bank bank = new Bank("MinBank", "1234", "12345678901234", "12340000000002", "12340000000009");
         Customer customer = new Customer("firstname", "lastname", "address", "phone");
         bank.addCustomer(customer);
@@ -42,7 +42,7 @@ public class JohannesTest {
     }
     // Test 2 - Overførsel af gyldigt beløb fra opsparingskonto til egen opsparingskonto
     @Test
-    public void canMakeATransferFromSavingsToSavingsAccount() {
+    public void canMakeATransferFromSavingsToSavingsAccount() throws Exception {
         Bank bank = new Bank("MinBank", "1234", "12345678901234", "12340000000002", "12340000000009");
         Customer customer = new Customer("firstname", "lastname", "address", "phone");
         bank.addCustomer(customer);
@@ -73,8 +73,8 @@ public class JohannesTest {
     }
 
     //Test 3 - Overførsel af for stort beløb fra opsparingskonto til egen lønkonto
-    @Test (expected = Exception.class)
-    public void can_Not_Make_A_Too_Large_Transfer_From_Savings_To_Current_Account() {
+    @Test (expected = NoOverdraftAllowedException.class)
+    public void can_Not_Make_A_Too_Large_Transfer_From_Savings_To_Current_Account() throws Exception {
         Bank bank = new Bank("MinBank", "1234", "12345678901234", "12340000000002", "12340000000009");
         Customer customer = new Customer("firstname", "lastname", "address", "phone");
         bank.addCustomer(customer);
@@ -90,7 +90,10 @@ public class JohannesTest {
 
         assertEquals(secondAccount, c.accountList.get(1));
 
-        firstAccount.deposit(3000);
+
+        Transaction putMoneyInAccount = new Transaction(bank, bank.getInterBankAccountNumber(),firstAccount.getAccountNumber(),3000);
+        bank.addTransaction(putMoneyInAccount);
+
 
         Transaction testTransaction = new Transaction(firstAccount, secondAccount, 4000);
         bank.addTransaction(testTransaction);
