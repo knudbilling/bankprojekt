a9444b00f5da2b67524188130dfd39693eb3c08d
Patricia <patriciakellvig@gmail.com>
2018-12-13 10:26:11 +0100

    Test rettet

--- test/PatriciaTest.java
+++ test/PatriciaTest.java
@@ -21,15 +21,15 @@ public class PatriciaTest {
     @Test(expected = NegativeAmountException.class)
     public void cantTransferNegativeAmounts() throws Exception{
        // Transaction transaction = null;
-        Bank bank=new Bank("MinBank","1234","12340000000001",
-                "12340000000002","12340000000009");
+        Bank bank=new Bank("MinBank","9800","98000000000001",
+                "98000000000002","98000000000009");
         Customer customer=new Customer("firstname","lastname","address","phone");
 
-        Account account = new SavingsAccount("12345678901234");
+        Account account = new SavingsAccount("98005678901234");
         bank.addAccount(customer, account);
 
-            Transaction transaction = new Transaction(bank,"12345678901234",
-                    "12340000000002",-50000);
+            Transaction transaction = new Transaction(bank,"98005678901234",
+                    "98000000000002",-50000);
             bank.addTransaction(transaction);
 
     }
@@ -41,26 +41,26 @@ public class PatriciaTest {
       * */
     @Test (expected = IllegalAccountException.class)
     public void cantTransferWithSavingsAccountToAnotherCustomer() throws Exception {
-        Bank bank=new Bank("MinBank","1234","12340000000001",
-                "12340000000002","12340000000009");
+        Bank bank=new Bank("MinBank","9800","98000000000001",
+                "98000000000002","98000000000009");
         Customer customer=new Customer("firstname","lastname","address","phone");
         bank.addCustomer(customer);
 
-        Account account = new CurrentAccount("12345678901234");
+        Account account = new CurrentAccount("98005678901234");
         bank.addAccount(customer, account);
 
         customer = new Customer("AndenKunde", "Lastname", "Norhavn", "mobil");
         bank.addCustomer(customer);
 
-        account = new SavingsAccount("12345678904321");
+        account = new SavingsAccount("98005678904321");
         bank.addAccount(customer, account);
 
         Transaction transaction = new Transaction(bank,bank.getCashAccountNumber(),
-                "12345678904321",50000);
+                "98005678904321",50000);
         bank.addTransaction(transaction);
 
-        transaction = new Transaction(bank,"12345678904321",
-                "12345678901234", 30000);
+        transaction = new Transaction(bank,"98005678904321",
+                "98005678901234", 30000);
         bank.addTransaction(transaction);
 
     }
@@ -72,26 +72,26 @@ public class PatriciaTest {
 
     @Test (expected = IllegalAccountException.class)
     public void cantTransferToInvalidAccountFromSavingsAccount() throws Exception {
-        Bank bank=new Bank("MinBank","1234","12340000000001",
-                "12340000000002","12340000000009");
+        Bank bank=new Bank("MinBank","9800","98000000000001",
+                "98000000000002","98000000000009");
         Customer customer=new Customer("førstekunde","lastname","address","phone");
         bank.addCustomer(customer);
 
-        Account account = new CurrentAccount("02039485761245");
+        Account account = new CurrentAccount("99009485761245");
         bank.addAccount(customer, account);
 
         customer = new Customer("AndenKunde", "Lastname", "Norhavn", "mobil");
         bank.addCustomer(customer);
 
-        account = new SavingsAccount("12345678904321");
+        account = new SavingsAccount("98005678904321");
         bank.addAccount(customer, account);
 
         Transaction transaction = new Transaction(bank,bank.getCashAccountNumber(),
-                "12345678904321",50000);
+                "98005678904321",50000);
         bank.addTransaction(transaction);
 
-        transaction = new Transaction(bank,"12345678904321",
-                "02039485761245", 30000);
+        transaction = new Transaction(bank,"98005678904321",
+                "99009485761245", 30000);
         bank.addTransaction(transaction);
 
     }
